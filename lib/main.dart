06a9a84614997e31010e5bfe9b784262f2dcc906
sharifc2016 <sharifc2016@gmail.com>
2021-09-04 12:38:42 +0600

    add dice images

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Dicee"),
+          backgroundColor: Colors.red,
+        ),
+        body: MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,4 +21,3 @@ class MyApp extends StatelessWidget {
     return Container();
   }
 }
-
